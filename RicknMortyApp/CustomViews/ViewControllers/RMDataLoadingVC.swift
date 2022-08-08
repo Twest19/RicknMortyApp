610c1cee8b98b70d779cd9b564eaa4e76268031c
Twest19 <67450649+Twest19@users.noreply.github.com>
2022-08-07 19:07:04 -0500

    Added a loadingVC

---
+++ RicknMortyApp/CustomViews/ViewControllers/RMDataLoadingVC.swift
@@ -0,0 +1,51 @@
+//
+//  RMDataLoadingVC.swift
+//  RicknMortyApp
+//
+//  Created by Tim West on 8/7/22.
+//
+
+import UIKit
+
+class RMDataLoadingVC: UIViewController {
+    
+    var containerView: UIView!
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { self.containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+    
+    
+    func showEmptyStateView(with message: String, and error: RMError, in view: UIView) {
+        let emptyStateView = RMErrorView(message: message, error: error)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+}
+
+
