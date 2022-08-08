15c4af18590dd003e14bb59663acbb5b3f947d1d
Twest19 <67450649+Twest19@users.noreply.github.com>
2022-08-07 19:06:52 -0500

    Refactored Network Manager to Result type

--- RicknMortyApp/Managers/NetworkManager.swift
+++ RicknMortyApp/Managers/NetworkManager.swift
@@ -7,13 +7,6 @@
 
 import UIKit
 
-enum NetworkManagerError: Error {
-    case badResponse(URLResponse?)
-    case badData
-    case badLocalUrl
-}
-
-
 class NetworkManager {
     
     static var shared = NetworkManager()
@@ -27,108 +20,61 @@ class NetworkManager {
         let config = URLSessionConfiguration.default
         session = URLSession(configuration: config)
     }
-    
-    
-    private func components() -> URLComponents {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "rickandmortyapi.com"
-        return components
-    }
-    
-    
-    private func characterComponents(with ID: String = "") -> URLComponents {
-        var components = components()
-        components.path = "/api/character/\(ID)"
-        return components
-    }
-    
-    
-    private func episodeComponents(episodeNum: String) -> URLComponents {
-        var components = components()
-        components.path = "/api/episode/\(episodeNum)"
-        return components
-    }
 
     // MARK: FETCHS ALL CHARACTERS AND ALLOWS FOR SEARCHING OF SPECIFICS
-    public func fetchCharacter(pageNum: Int, searchBarText: String = "", completion: @escaping (RMResults?, Error?) -> Void) {
-        var components = characterComponents()
-        let pages = URLQueryItem(name: "page", value: String(pageNum))
-        let userCharacterSearch = URLQueryItem(name: "name", value: searchBarText)
-        
-        // Provides proper query items for URL
-        if searchBarText == "" || searchBarText == " " {
-            components.queryItems = [pages]
-        } else {
-            components.queryItems = [pages, userCharacterSearch]
-        }
+    public func getCharacters(pageNum: Int, searchBarText: String = "", completion: @escaping (Result<RMResults, RMError>) -> Void) {
+        let url = CharacterURLManager.shared.createCharacterURL(pageNum: pageNum, searchBarText: searchBarText)
         
         // Checks URL
-        guard let url = components.url else {
-            print("Error: BAD URL, check URL...")
+        guard let url = url else {
+            completion(.failure(.invalidCharacter))
             return
         }
-        
-        print(url)
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
             // Handle any errors here:
             if let error = error {
                 print("Error retrieving data, error: \(String(describing: error))")
-                completion(nil, error)
+                completion(.failure(.unableToComplete))
                 return
             }
 
             // Handle response errors here:
             guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                print("http response error \(String(describing: response))")
-                completion(nil, NetworkManagerError.badResponse(response))
+                print("When enter invalid search")
+                completion(.failure(.responseError))
                 return
             }
             
             // Handle data errors here:
             guard let data = data else {
-                completion(nil, NetworkManagerError.badData)
+                completion(.failure(.badDataError))
                 return
             }
-            
             
             // Decode JSON into Model
             let decoder = JSONDecoder()
             
             do {
                 let response = try decoder.decode(RMResults.self, from: data)
-                completion(response, nil)
+                completion(.success(response))
             } catch let error {
                 print("ERROR DECODING")
                 print(error)
-                completion(nil, error)
+                completion(.failure(.badDataError))
             }
         }
         task.resume()
     }
     
-    
-    func textEpisodeCharacter(with characterIDs: String) {
-        let components = characterComponents(with: characterIDs)
-        
-        // Checks URL
-        guard let url = components.url else {
-            print("Error: BAD URL, check URL...")
-            return
-        }
-        
-        print(url)
-    }
-    
     // MARK: REQUEST FOR SPECIFIC EPISODES CHARACTER LIST
-    public func getEpisodeCharacters(with characterIDs: String, completion: @escaping ([RMCharacter]?, Error?) -> (Void)) {
-        let components = characterComponents(with: characterIDs)
+    public func getEpisodeCharacters(with characterIDs: String, completion: @escaping (Result<[RMCharacter], RMError>) -> Void) {
+        let components = CharacterURLManager.shared.characterComponents(with: characterIDs)
         
         // Checks URL
         guard let url = components.url else {
-            print("Error: BAD URL, check URL...")
+            completion(.failure(.invalidCharacter))
             return
         }
         
@@ -139,69 +85,68 @@ class NetworkManager {
             // Handle any errors here:
             if let error = error {
                 print("Error retrieving data, error: \(String(describing: error))")
-                completion(nil, error)
+                completion(.failure(.unableToComplete))
                 return
             }
 
             // Handle response errors here:
             guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
                 print("http response error \(String(describing: response))")
-                completion(nil, NetworkManagerError.badResponse(response))
+                completion(.failure(.responseError))
                 return
             }
             
             // Handle data errors here:
             guard let data = data else {
-                completion(nil, NetworkManagerError.badData)
+                completion(.failure(.badDataError))
                 return
             }
-            
             
             // Decode JSON into Model
             let decoder = JSONDecoder()
             
             do {
                 let response = try decoder.decode([RMCharacter].self, from: data)
-                completion(response, nil)
+                completion(.success(response))
             } catch let error {
                 print("ERROR DECODING")
                 print(error)
-                completion(nil, error)
+                completion(.failure(.badDataError))
             }
         }
         task.resume()
     }
 
     // MARK: REQUEST FOR EPISODE DATA
-    public func getEpisodeData(episodes: String, completion: @escaping ([Episode]?, Error?) -> (Void)) {
-        let components = episodeComponents(episodeNum: episodes)
+    public func getEpisodeData(episodes: String, completion: @escaping (Result<[Episode], RMError>) -> Void) {
+        let components = EpisodeURLManager.shared.episodeComponents(episodeNum: episodes)
 
         // Checks URL
         guard let url = components.url else {
-            print("Error: BAD URL, check URL...")
+            print("NOT A CHARACTER!!!!!")
+            completion(.failure(.invalidCharacter))
             return
         }
-
 
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
 
             // Handle any errors here:
             if let error = error {
                 print("Error retrieving data, error: \(String(describing: error))")
-                completion(nil, error)
+                completion(.failure(.unableToComplete))
                 return
             }
 
             // Handle response errors here:
             guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
                 print("http response error \(String(describing: response))")
-                completion(nil, NetworkManagerError.badResponse(response))
+                completion(.failure(.responseError))
                 return
             }
 
             // Handle data errors here:
             guard let data = data else {
-                completion(nil, NetworkManagerError.badData)
+                completion(.failure(.badDataError))
                 return
             }
             
@@ -211,44 +156,42 @@ class NetworkManager {
             // Decode JSON into Episode Model
             do {
                 let response = try decoder.decode([Episode].self, from: data)
-                completion(response, nil)
+                completion(.success(response))
             } catch let error {
                 print(error)
-                completion(nil, error)
+                completion(.failure(.badDataError))
             }
         }
         task.resume()
     }
     
-    // MARK: DOWNLOADING IMAGES
-    public func downloadCharImage(character: URL, completion: @escaping (Data?, Error?) -> (Void)) {
+    // MARK: DOWNLOADING IMAGE AND CACHE
+    public func downloadAndCacheImageFor(character: URL, completion: @escaping (Result<Data, RMError>) -> Void) {
         
         // Checks if Image has been cached and passes it back if so...
         if let imageData = images.object(forKey: character.absoluteString as NSString) {
-//            print("using cached image...")
-            completion(imageData as Data, nil)
+            completion(.success(imageData as Data))
             return
         }
         
         let task = session.downloadTask(with: character) { localurl, urlresponse, error in
-//            print("...Not a cached Image")
             // Handle any errors here:
             if let error = error {
                 print("Error retrieving data, error: \(String(describing: error))")
-                completion(nil, error)
+                completion(.failure(.unableToComplete))
                 return
             }
 
             // Handle response errors here:
             guard let httpResponse = urlresponse as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
                 print("http response error \(String(describing: urlresponse))")
-                completion(nil, NetworkManagerError.badResponse(urlresponse))
+                completion(.failure(.responseError))
                 return
             }
             
             // Handle data errors here:
             guard let localurl = localurl else {
-                completion(nil, NetworkManagerError.badLocalUrl)
+                completion(.failure(.badLocalUrl))
                 return
             }
             
@@ -256,20 +199,19 @@ class NetworkManager {
             do {
                 let myData = try Data(contentsOf: localurl)
                 self.images.setObject(myData as NSData, forKey: character.absoluteString as NSString)
-                completion(myData, nil)
-            } catch let error {
-                completion(nil, error)
+                completion(.success(myData))
+            } catch {
+                completion(.failure(.badDataError))
             }
-        
         }
         task.resume()
-        
     }
     
-    public func image(name: String, completion: @escaping (Data?, Error?) -> (Void)) {
+    
+    public func downloadImageUsing(URLString: String, completion: @escaping (Result<Data, RMError>) -> Void) {
 
-        if let url = URL(string: name) {
-            downloadCharImage(character: url, completion: completion)
+        if let url = URL(string: URLString) {
+            downloadAndCacheImageFor(character: url, completion: completion)
         }
     }
 }
