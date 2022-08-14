347889d9495e2352bfd6368993061b022329b9dd
Twest19 <67450649+Twest19@users.noreply.github.com>
2022-08-14 00:55:54 -0500

    Updated EpisodeVC
    Added the meat and potatoes to the EpisodeVC. Still need to add delegates and sections.

--- RicknMortyApp/Screens/RMEpisodeVC.swift
+++ RicknMortyApp/Screens/RMEpisodeVC.swift
@@ -7,21 +7,70 @@
 
 import UIKit
 
-class RMEpisodeVC: UIViewController {
+class RMEpisodeVC: RMDataLoadingVC {
     
     let tableView = RMEpisodeTableView()
+    
+    var episode: [Episode] = []
+    
+    var totalPages = 0
+    var currentPage = 1
+    
+    var isLoadingEpisodeData: Bool = false
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
+        configureNavBar()
         configureTableView()
+        getEpisodeData(pageNum: currentPage)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    
+    func getEpisodeData(pageNum: Int) {
+        isLoadingEpisodeData = true
+        showLoadingView()
+        
+        NetworkManager.shared.getEpisode(pageNum: pageNum) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let episode):
+                print(episode.info.pages)
+                self.totalPages = episode.info.pages
+                self.updateUI(episode: episode.results)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showErrorView(with: error, in: self.view)
+                    self.isLoadingEpisodeData = false
+                }
+                return
+            }
+            self.isLoadingEpisodeData = false
+        }
+    }
+    
+    
+    private func updateUI(episode: [Episode]) {
+        self.episode.append(contentsOf: episode)
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     
     private func configureTableView() {
-        
-//        tableView.delegate = self
-//        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
+//        tableView.rowHeight = UITableView.automaticDimension
         
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -30,5 +79,55 @@ class RMEpisodeVC: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+    
+    
+    private func configureNavBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .systemCyan
+        title = "Episodes"
+        view.backgroundColor = .systemBackground
+    }
+}
 
+
+extension RMEpisodeVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UIView.animate(withDuration: 0.3) {
+            self.tableView.performBatchUpdates(nil)
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = self.tableView.cellForRow(at: indexPath) as? EpisodeCell {
+            cell.hideEpisodeHiddenView()
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if currentPage < totalPages && indexPath.row == episode.count - 1 {
+            guard !isLoadingEpisodeData else { return }
+            currentPage += 1
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.getEpisodeData(pageNum: self.currentPage)
+            }
+        }
+    }
+}
+
+
+extension RMEpisodeVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episode.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: EpisodeCell.reuseID, for: indexPath) as! EpisodeCell
+        cell.updateCell(with: episode[indexPath.row])
+        return cell
+    }
 }
