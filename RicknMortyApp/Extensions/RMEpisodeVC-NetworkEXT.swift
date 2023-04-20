6462beb36d69710b146564600b0ce27ca387106e
Twest19 <67450649+Twest19@users.noreply.github.com>
2023-04-20 13:41:58 -0500

    Abstracted Episode Network Calls
    Put EpisodeVC network call in its own extension for better organization.

---
+++ RicknMortyApp/Extensions/RMEpisodeVC-NetworkEXT.swift
@@ -0,0 +1,36 @@
+//
+//  RMEpisodeVC-NetworkEXT.swift
+//  RicknMortyApp
+//
+//  Created by Tim West on 4/20/23.
+//
+
+import Foundation
+
+extension RMEpisodeVC {
+    
+    func fetchEpisodeData(pageNum: Int) {
+        isLoadingEpisodeData = true
+        showLoadingView()
+        
+        NetworkManager.shared.getEpisode(pageNum: pageNum) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let episode):
+                self.totalPages = episode.info.pages
+                self.dataStore.saveEpisodesBySeason(episodes: episode.results)
+                self.updateUI(with: self.dataStore.getEpisodes())
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showErrorView(with: error, in: self.view)
+                    self.isLoadingEpisodeData = false
+                }
+                return
+            }
+            self.isLoadingEpisodeData = false
+        }
+    }
+    
+}
