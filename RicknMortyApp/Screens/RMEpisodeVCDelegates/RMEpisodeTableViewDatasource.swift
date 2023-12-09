6d23726ea98dcd077c202943cdb7d2f8bce0a183
Tim West <67450649+Twest19@users.noreply.github.com>
2023-12-09 00:57:34 -0600

    More MVC clean up on episode screen

---
+++ RicknMortyApp/Screens/RMEpisodeVCDelegates/RMEpisodeTableViewDatasource.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisodeTableViewDatasource.swift
+//  RicknMortyApp
+//
+//  Created by Tim West on 12/8/23.
+//
+
+import Foundation
---
+++ RicknMortyApp/Screens/RMEpisodeVCDelegates/RMEpisodeTableViewDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisodeTableViewDelegate.swift
+//  RicknMortyApp
+//
+//  Created by Tim West on 12/8/23.
+//
+
+import Foundation
